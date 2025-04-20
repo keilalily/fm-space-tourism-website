8e11bdbe499d4d6b46dae85d4a2dda9c437623f9
keilalily <kylamarieangeles@gmail.com>
2025-04-20 16:01:09 +0800

    feature/setup-tailwindcss
    Setup Tailwind CSS.
    Added custom styles and pre-set text styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import './App.css'
-
 function App() {
   return (
     <>
-      <h1>Welcome to Space Tourism Website!</h1>
+      <h1 className='heading-xxl'>Welcome to Space Tourism Website!</h1>
     </>
   )
 }
