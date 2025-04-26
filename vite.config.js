011ccef22774e43c32dc34bff2958ab8623d3fa4
keilalily <kylamarieangeles@gmail.com>
2025-04-26 18:42:01 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/fm-space-tourism-website/',
   plugins: [
     react(),
     tailwindcss(),
