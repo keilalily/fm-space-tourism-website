bcb8928ae4a946aff9e509f6d34de7ee78f61119
keilalily <kylamarieangeles@gmail.com>
2025-04-26 18:34:57 +0800

    Updates

---
+++ assets/Home-D9fQc6Pf.js
@@ -0,0 +1,16 @@
+import{j as e}from"./index-89E2kIsx.js";import{u as r,P as l,b as n}from"./useResponsiveMedia-Conzuly0.js";function o(){const t=r(n.home);return e.jsx(l,{backgroundImage:t,children:e.jsxs("div",{className:`\r
+                    flex flex-col items-center \r
+                    w-full h-full grow p-6 md:p-32 md:gap-6 lg:p-24 lg:flex-row lg:items-end xl:p-32`,children:[e.jsxs("div",{className:"flex flex-col items-center gap-6 h-full md:gap-8 lg:items-start lg:flex-1",children:[e.jsx("p",{className:`\r
+                        font-barlow-condensed tracking-15 text-[1rem] uppercase text-blue-light\r
+                        md:text-[1.75rem]`,children:"So, you want to travel to"}),e.jsx("h1",{className:"font-bellefair text-[5rem] md:text-[9rem] leading-none uppercase text-white",children:"Space"}),e.jsx("p",{className:`\r
+                        font-barlow leading-[1.8] text-[15px] text-center text-blue-light\r
+                        md:text-[1rem] lg:text-left xl:text-[1.125rem]`,children:"Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!"})]}),e.jsx("div",{className:"flex flex-1 items-center justify-center w-full h-full lg:justify-end",children:e.jsx("a",{href:"/destination",className:"flex items-center justify-center h-full",children:e.jsx("button",{type:"button",className:`\r
+                                flex items-center justify-center\r
+                                w-36 h-36 rounded-full bg-white\r
+                                text-blue-dark uppercase\r
+                                shadow-[0_0_0_88px_rgba(255,255,255,0)]\r
+                                hover:shadow-[0_0_0_88px_rgba(255,255,255,0.10)]\r
+                                hover:cursor-pointer\r
+                                transition-500\r
+                                font-bellefair text-[1.125rem]\r
+                                md:w-68 md:h-68 md:text-[32px]`,children:"Explore"})})})]})})}export{o as default};
