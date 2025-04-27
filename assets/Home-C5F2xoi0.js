240d6d2cb84fc7754025abe6d9481992c5ec043e
keilalily <kylamarieangeles@gmail.com>
2025-04-27 10:42:25 +0800

    Updates

--- assets/Home-C5F2xoi0.js
+++ assets/Home-C5F2xoi0.js
@@ -1,10 +1,10 @@
-import{j as e}from"./index-CVjQjRoj.js";import{u as r,P as l,b as n}from"./useResponsiveMedia-Df1lIefW.js";function o(){const t=r(n.home);return e.jsx(l,{backgroundImage:t,children:e.jsxs("div",{className:`\r
+import{j as e,L as l}from"./index-DDUVcn9P.js";import{u as r,P as n,b as s}from"./useResponsiveMedia-DViYHIto.js";function i(){const t=r(s.home);return e.jsx(n,{backgroundImage:t,children:e.jsxs("div",{className:`\r
                     flex flex-col items-center \r
                     w-full h-full grow p-6 md:p-32 md:gap-6 lg:p-24 lg:flex-row lg:items-end xl:p-32`,children:[e.jsxs("div",{className:"flex flex-col items-center gap-6 h-full md:gap-8 lg:items-start lg:flex-1",children:[e.jsx("p",{className:`\r
                         font-barlow-condensed tracking-15 text-[1rem] uppercase text-blue-light\r
                         md:text-[1.75rem]`,children:"So, you want to travel to"}),e.jsx("h1",{className:"font-bellefair text-[5rem] md:text-[9rem] leading-none uppercase text-white",children:"Space"}),e.jsx("p",{className:`\r
                         font-barlow leading-[1.8] text-[15px] text-center text-blue-light\r
-                        md:text-[1rem] lg:text-left xl:text-[1.125rem]`,children:"Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!"})]}),e.jsx("div",{className:"flex flex-1 items-center justify-center w-full h-full lg:justify-end",children:e.jsx("a",{href:"/destination",className:"flex items-center justify-center h-full",children:e.jsx("button",{type:"button",className:`\r
+                        md:text-[1rem] lg:text-left xl:text-[1.125rem]`,children:"Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!"})]}),e.jsx("div",{className:"flex flex-1 items-center justify-center w-full h-full lg:justify-end",children:e.jsx(l,{to:"/destination",className:"flex items-center justify-center h-full",children:e.jsx("button",{type:"button",className:`\r
                                 flex items-center justify-center\r
                                 w-36 h-36 rounded-full bg-white\r
                                 text-blue-dark uppercase\r
@@ -13,4 +13,4 @@ import{j as e}from"./index-CVjQjRoj.js";import{u as r,P as l,b as n}from"./useRe
                                 hover:cursor-pointer\r
                                 transition-500\r
                                 font-bellefair text-[1.125rem]\r
-                                md:w-68 md:h-68 md:text-[32px]`,children:"Explore"})})})]})})}export{o as default};
+                                md:w-68 md:h-68 md:text-[32px]`,children:"Explore"})})})]})})}export{i as default};
